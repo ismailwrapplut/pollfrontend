09e0b79b613363891af721721f3bd6906bfb8d81
Mohd Ismail <smileweb.developer@gmail.com>
2023-07-31 23:25:12 +0530

    dsf

--- src/baseURL.js
+++ src/baseURL.js
@@ -1,2 +1,2 @@
 export const baseURL = "https://pollbackend.onrender.com/api/v1";
-export const frontendURL = "http://localhost:3000";
+export const frontendURL = "https://symphonious-chebakia-8a62ab.netlify.app";
